8953b26f951fb483b0f83936b72c3fe7185c3197
vporta <vporta7@gmail.com>
2016-07-30 19:12:18 -0400

    mlab

--- config/db.js
+++ config/db.js
@@ -1,5 +1,16 @@
 var mongoose = require('mongoose');
 var scrape_db = 'mongodb://localhost/scrape_db';
+
+// Database configuration with mongoose
+// var scrape_db = 'mongodb://localhost/week18mongoose';
+// var mlabDatabaseUri ='mongodb://test:test217@ds061721.mlab.com:61721/heroku_d103gdp1';
+// if (process.env.MONGODB_URI) {
+//   console.log('THIS IS THE HEROKU MONGODB URI =====> ' + process.env.MONGODB_URI);
+//   mongoose.connect(process.env.MONGODB_URI);
+// } else {
+//   mongoose.connect(scrape_db);
+// }
+
 //Database configuration
 mongoose.connect(scrape_db);
 
--- server.js
+++ server.js
@@ -8,7 +8,9 @@ var bcrypt = require('bcryptjs');
 var cheerio = require('cheerio');
 var db = require('./config/db');
 var path = require('path');
-//remove handlebars files and add html file
+var mongoose = require('mongoose');
+
+
 var app = express();
 
 app.use(logger('dev'));
@@ -18,17 +20,12 @@ app.use(bodyParser.urlencoded({
 
 app.use(express.static(process.cwd() + '/public'));
 app.use(express.static(__dirname + '/public'));
-// app.set('views', path.join(__dirname, 'views'));
 
 app.use(session({ secret: 'app', cookie: { maxAge: 6000000 }}));
 
 //Override with POST having ?_method=DELETE
 app.use(methodOverride('_method'))
-// var exphbs = require('express-handlebars');
-// app.engine('handlebars', exphbs({
-//     defaultLayout: 'main'
-// }));
-// app.set('view engine', 'handlebars');
+
 
 var notes_controllers = require('./controllers/notes_controller.js');
 var titles_controllers = require('./controllers/titles_controller.js');
@@ -37,9 +34,27 @@ app.use('/', notes_controllers);
 app.use('/', titles_controllers);
 
 
+// Database configuration with mongoose
+// var databaseUri = 'mongodb://localhost/week18mongoose';
+// var mlabDatabaseUri ='mongodb://test:test217@ds061721.mlab.com:61721/heroku_d103gdp1';
+// if (process.env.MONGODB_URI) {
+//   console.log('THIS IS THE HEROKU MONGODB URI =====> ' + process.env.MONGODB_URI);
+//   mongoose.connect(process.env.MONGODB_URI);
+// } else {
+//   mongoose.connect(databaseUri);
+// }
 
+// var db = mongoose.connection;
 
+// // show any mongoose errors
+// db.on('error', function(err) {
+//   console.log('Mongoose Error: ', err);
+// });
 
+// // once logged in to the db through mongoose, log a success message
+// db.once('open', function() {
+//   console.log('Mongoose connection successful.');
+// });
 
 app.listen(3000, function() {
   console.log('App running on port 3000!');
