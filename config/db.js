723e01c3de7010803b8de0d9a463c4a4a2e232ff
vporta <vporta7@gmail.com>
2016-07-30 19:43:17 -0400

    setup mlab connection

--- config/db.js
+++ config/db.js
@@ -1,28 +1,51 @@
 var mongoose = require('mongoose');
-var scrape_db = 'mongodb://localhost/scrape_db';
 
 // Database configuration with mongoose
-// var scrape_db = 'mongodb://localhost/week18mongoose';
-// var mlabDatabaseUri ='mongodb://test:test217@ds061721.mlab.com:61721/heroku_d103gdp1';
-// if (process.env.MONGODB_URI) {
-//   console.log('THIS IS THE HEROKU MONGODB URI =====> ' + process.env.MONGODB_URI);
-//   mongoose.connect(process.env.MONGODB_URI);
-// } else {
-//   mongoose.connect(scrape_db);
-// }
+
+var scrape_db = 'mongodb://localhost/scrape_db';
+
+if (process.env.MONGODB_URI) {
+  console.log('THIS IS THE HEROKU MONGODB URI =====> ' + process.env.MONGODB_URI);
+  mongoose.connect(process.env.MONGODB_URI);
+} else {
+  mongoose.connect(scrape_db);
+}
 
 //Database configuration
-mongoose.connect(scrape_db);
+// mongoose.connect(scrape_db);
+  
 
 var db = mongoose.connection;
+
+db.on('error', function(err) {
+  console.log('Database Error:', err);
+});
 
 db.once('open', function() {
   // we're connected!
   console.log('connected to ' + scrape_db);
 });
 
-db.on('error', function(err) {
-  console.log('Database Error:', err);
-});
-
 module.exports = mongoose;
+
+// Database configuration with mongoose
+// var databaseUri = 'mongodb://localhost/week18day3mongoose';
+// var mlabDatabaseUri ='mongodb://test:test217@ds061721.mlab.com:61721/heroku_d103gdp1';
+// if (process.env.MONGODB_URI) {
+//   console.log('THIS IS THE HEROKU MONGODB URI =====> ' + process.env.MONGODB_URI);
+//   mongoose.connect(process.env.MONGODB_URI);
+// } else {
+//   mongoose.connect(databaseUri);
+// }
+
+// var db = mongoose.connection;
+
+// // show any mongoose errors
+// db.on('error', function(err) {
+//   console.log('Mongoose Error: ', err);
+// });
+
+// // once logged in to the db through mongoose, log a success message
+// db.once('open', function() {
+//   console.log('Mongoose connection successful.');
+// });
