616f498b9ae23c92e9bf82cca09d90c12799e924
vporta <vporta7@gmail.com>
2016-07-31 17:42:39 -0400

    changed set port

--- server.js
+++ server.js
@@ -56,8 +56,10 @@ db.once('open', function() {
   // we're connected!
   console.log('connected to ' + scrape_db);
 });
+// app.listen(process.env.PORT || 3000, function(){
+//   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+// });
 
-
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log('App running on port 3000!');
 });
