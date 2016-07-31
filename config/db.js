a631b1a7a7ee581b21bd560a0fa6cc1bdbfa9e94
vporta <vporta7@gmail.com>
2016-07-30 20:28:48 -0400

    m

--- config/db.js
+++ config/db.js
@@ -4,12 +4,12 @@ var mongoose = require('mongoose');
 
 var scrape_db = 'mongodb://localhost/scrape_db';
 
-if (process.env.MONGODB_URI) {
-  console.log('THIS IS THE HEROKU MONGODB URI =====> ' + process.env.MONGODB_URI);
-  mongoose.connect(process.env.MONGODB_URI);
-} else {
-  mongoose.connect(scrape_db);
-}
+// if (process.env.MONGODB_URI) {
+//   console.log('THIS IS THE HEROKU MONGODB URI =====> ' + process.env.MONGODB_URI);
+//   mongoose.connect(process.env.MONGODB_URI);
+// } else {
+//   mongoose.connect(scrape_db);
+// }
 
 //Database configuration
 // mongoose.connect(scrape_db);
